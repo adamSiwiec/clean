941bb51a350bf319875c42be5301482124739249
Adam Siwiec <brickmaster01@gmail.com>
2016-12-26 18:25:17 -0600

    removed extraneous links and images

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 require('./css/materialize.min.css')
 require('./css/style.css')
 require('./img/clean1.jpg')
-require('./img/clean2.jpg')
-require('./img/clean3.jpg')
 require('./js/materialize.min.js')
 require('./js/border.min.js')
 require('./js/init.js')
